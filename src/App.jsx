878546d7060edf850c81fa9197cf596df46d1b6f
tracymuzaki <tracymuzaki12@gmail.com>
2024-07-08 04:40:14 +0300

    added the homepage

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Shop from "./components/Shop";
 import Checkout from "./components/Checkout";
 import Footer from "./components/Footer";
 import "./App.css";
+import HomePage from "./components/HomePage";
 
 const App = () => {
   return (
@@ -12,7 +13,8 @@ const App = () => {
       <div className="app">
         <Navbar />
         <Routes>
-          <Route path="/" element={<Shop />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/shop" element={<Shop />} />
           <Route path="/checkout" element={<Checkout />} />
         </Routes>
         <Footer />
