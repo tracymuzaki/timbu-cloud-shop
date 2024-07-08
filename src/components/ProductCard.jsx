002c8fb88363400e2c1aacb2a054c204a9f78bd5
tracymuzaki <tracymuzaki12@gmail.com>
2024-07-08 03:07:48 +0300

    added add to cart button

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -8,6 +8,7 @@ const ProductCard = ({ product }) => {
       <h2>{product.name}</h2>
       <p>{product.description}</p>
       <p>{product.price}</p>
+      <button>Add to Cart</button>
     </div>
   );
 };
