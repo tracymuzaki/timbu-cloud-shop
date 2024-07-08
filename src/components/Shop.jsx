302404cbc4e7a8f47d25d617fcd99d4ceb2d4618
tracymuzaki <tracymuzaki12@gmail.com>
2024-07-08 03:29:26 +0300

    added product page design

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -17,6 +17,48 @@ const products = [
     price: "$20",
     image: "link-to-image",
   },
+  {
+    id: 3,
+    name: "Product 3",
+    description: "Description for product 3",
+    price: "$10",
+    image: "link-to-image",
+  },
+  {
+    id: 4,
+    name: "Product 4",
+    description: "Description for product 4",
+    price: "$20",
+    image: "link-to-image",
+  },
+  {
+    id: 5,
+    name: "Product 5",
+    description: "Description for product 5",
+    price: "$10",
+    image: "link-to-image",
+  },
+  {
+    id: 6,
+    name: "Product 6",
+    description: "Description for product 6",
+    price: "$20",
+    image: "link-to-image",
+  },
+  {
+    id: 7,
+    name: "Product 7",
+    description: "Description for product 7",
+    price: "$10",
+    image: "link-to-image",
+  },
+  {
+    id: 8,
+    name: "Product 8",
+    description: "Description for product 8",
+    price: "$20",
+    image: "link-to-image",
+  },
   // Add more products as needed
 ];
 
