87a33abd1a9e99a3e759721cd48a09fba8e45b66
tracymuzaki <tracymuzaki12@gmail.com>
2024-07-08 05:51:05 +0300

    updated content on the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import "./Navbar.css";
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="navbar-logo">Timbu Cloud Shop</div>
+      <div className="navbar-logo">Timbu</div>
       <div className="navbar-links">
         <Link to="/">Home</Link>
         <Link to="/shop">Products</Link>
